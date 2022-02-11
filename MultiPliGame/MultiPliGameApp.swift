8112918977eaeac574778deefc600cb0b7f2b4b4
moet-stein <0101.mste@gmail.com>
2022-02-11 11:39:30 +0100

    Initial Commit

---
+++ MultiPliGame/MultiPliGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPliGameApp.swift
+//  MultiPliGame
+//
+//  Created by Moe Steinmueller on 11.02.22.
+//
+
+import SwiftUI
+
+@main
+struct MultiPliGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
